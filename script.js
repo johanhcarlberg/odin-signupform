e26d8c3b07a22b1b24c5f7f8334df3025a73f852
JSHC <johan.hansson.carlberg@gmail.com>
2022-12-08 09:53:52 +0100

    Disable default html form validation

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 const passwordInput = document.querySelector("#password");
 const passwordRepeatInput = document.querySelector("#password-repeat");
+const form = document.querySelector("form.signup-form");
 
 passwordInput.addEventListener('focusout', checkPasswords);
 passwordRepeatInput.addEventListener('focusout', checkPasswords);
+form.addEventListener('submit', onSubmit)
+
+function onSubmit(e) {
+    checkPasswords();
+    if(!form.checkValidity()) {
+        e.preventDefault();
+    }
+    
+}
 
 function checkPasswords(event) {
     const pwString = passwordInput.value;
@@ -11,13 +21,13 @@ function checkPasswords(event) {
     if (passwordInput.validity.valueMissing) {
         passwordInput.classList.add("error");
         pwErrorSpan.textContent = "*Password is empty";
+        passwordInput.setCustomValidity("Password is empty");
     } else if (pwString !== pwRepeatString) {
         passwordInput.classList.add("error");
         passwordRepeatInput.classList.add("error");
         pwErrorSpan.textContent = "*Passwords do not match";
-    }
-
-    if (passwordInput.validity.valid && passwordRepeatInput.validity.valid) {
+        passwordInput.setCustomValidity("Passwords do not match");
+    } else {
         passwordInput.classList.remove("error");
         passwordRepeatInput.classList.remove("error");
         pwErrorSpan.textContent = "";
