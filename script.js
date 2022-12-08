ce5b1c9dd0d9f3b24539fcef6eb178c3c46fa938
JSHC <johan.hansson.carlberg@gmail.com>
2022-12-08 10:07:51 +0100

    Add custom validity to names, email and password fields

--- script.js
+++ script.js
@@ -7,11 +7,13 @@ passwordRepeatInput.addEventListener('focusout', checkPasswords);
 form.addEventListener('submit', onSubmit)
 
 function onSubmit(e) {
+    checkFirstname();
+    checkLastName();
+    checkEmail();
     checkPasswords();
     if(!form.checkValidity()) {
         e.preventDefault();
     }
-    
 }
 
 function checkPasswords(event) {
@@ -31,5 +33,48 @@ function checkPasswords(event) {
         passwordInput.classList.remove("error");
         passwordRepeatInput.classList.remove("error");
         pwErrorSpan.textContent = "";
+        passwordInput.setCustomValidity("");
+    }
+}
+
+function checkFirstname(event) {
+    const firstNameInput = document.querySelector("#first-name");
+    const firstNameErrorSpan = document.querySelector("#first-name~span");
+    if (firstNameInput.validity.valueMissing) {
+        firstNameInput.classList.add("error");
+        firstNameErrorSpan.textContent = "*First name is empty";
+        firstNameInput.setCustomValidity("First name is empty");
+    } else {
+        firstNameInput.classList.remove("error");
+        firstNameErrorSpan.textContent = "";
+        firstNameInput.setCustomValidity("");
+    }
+}
+
+function checkLastName(event) {
+    const lastNameInput = document.querySelector("#last-name");
+    const lastNameErrorSpan = document.querySelector("#last-name~span");
+    if (lastNameInput.validity.valueMissing) {
+        lastNameInput.classList.add("error");
+        lastNameErrorSpan.textContent = "*Last name is empty";
+        lastNameInput.setCustomValidity("Last name is empty");
+    } else {
+        lastNameInput.classList.remove("error");
+        lastNameErrorSpan.textContent = "";
+        lastNameInput.setCustomValidity("");
+    }
+}
+
+function checkEmail(event) {
+    const emailInput = document.querySelector("#email");
+    const emailErrorSpan = document.querySelector("#email~span");
+    if (emailInput.validity.valueMissing) {
+        emailInput.classList.add("error");
+        emailErrorSpan.textContent = "*Email is empty";
+        emailInput.setCustomValidity("Email is empty");
+    } else {
+        emailInput.classList.remove("error");
+        emailErrorSpan.textContent = "";
+        emailInput.setCustomValidity("");
     }
 }
